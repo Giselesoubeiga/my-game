3cfb41313120b21d9ddaafbc4176421ef110eeca
Gisele <giskey11@gmail.com>
2020-11-04 16:18:32 -0600

    scrypt work in progress

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+//Selection of HTML elements
+const btnStart = document.querySelector("#btn-start");
 const gameSection = document.querySelector("#game-section");
 const questionArea = document.getElementById("questionArea");
 let titleGame = document.getElementById("title-game");
 
+//QuestioNs list in order
 var questions = {
   q1: "Hay un muchacho y una muchacha en la clase de matemáticas.",
   q2: "El maestro dice que los estudiantes necesitan un lápiz.",
@@ -11,18 +14,43 @@ var questions = {
     "Mateo no tiene un lápiz, no tiene una hoja de papel, y no tiene un libro.",
   q6: "El maestro está furioso.",
 };
-const goodResponses = ["q1", "q2", "q3", "q4", "q5", "q6"];
+
+//Array of good answers
+const goodAnswer = ["q1", "q2", "q3", "q4", "q5", "q6"];
 var reponses = [];
 
-//display game
+//Start the game
 initGame();
 
-function initGame() {
-  titleGame.innerText = "Normal questions";
-  displayGame(questions);
-  satrtShuffleGame();
+
+//Function to diplay normal questions
+function displayNormalQuestion() {
+  const answers = [];
+
+  console.log (questions)
+  console.log(Object.keys(questions))
+  
+  Object.keys(questions).map((ele) => {
+    answers.push(`<div class="p-2 w-full text-sm"> 
+                    <span class="p-1 px-2 mr-3 rounded text-white bg-blue-600 font-bold">${ele} </span>
+                        <label>${questions[ele]}</label>
+                    </div>`);
+  });
+
+  questionArea.innerHTML = answers.join("");
 }
 
+//Function to display the game questions
+function initGame() {
+  displayNormalQuestion();
+}
+
+// Start the game with shuffling questions
+btnStart.addEventListener("click", (e) => {
+  shuffleQuestions(questions);
+});
+
+//Check questions
 gameSection.addEventListener("click", (e) => {
   let check = e.target;
   if (check.getAttribute("class") == "checkbox checkbox-question-item") {
@@ -36,21 +64,23 @@ gameSection.addEventListener("click", (e) => {
     console.log(reponses);
 
     var is_same =
-      goodResponses.length == reponses.length &&
-      goodResponses.every(function (element, index) {
+      goodAnswer.length == reponses.length &&
+      goodAnswer.every(function (element, index) {
         return element === reponses[index];
       });
     if (is_same) {
-      alert("is_same");
+      titleGame.innerText = "Good order";
+    } else if (reponses.length == 6) {
+      titleGame.innerText = "Wrong order";
     }
   }
 });
 
+//Function to generate questions in HTML
 function displayGame(questions) {
   const answers = [];
   Object.keys(questions).map((ele) => {
     answers.push(`<div class="p-2 w-full text-sm"> 
-                    <span class="p-1 px-2 mr-3 rounded text-white bg-blue-600 font-bold">${ele} </span>
                         <input type="checkbox" class="checkbox checkbox-question-item" id="${ele}">
                         <label>${questions[ele]}</label>
                     </div>`);
@@ -58,10 +88,8 @@ function displayGame(questions) {
 
   questionArea.innerHTML = answers.join("");
 }
-function satrtShuffleGame() {
-  shuffleQuestions(questions);
-}
 
+// Function to shuffle question object
 function shuffleQuestions(questions) {
   let mkeys = Object.keys(questions);
   let infos = {};
@@ -72,6 +100,7 @@ function shuffleQuestions(questions) {
   displayGame(infos);
 }
 
+// Function to shuffle Array
 function shuffle(a) {
   var j, x, i;
   for (i = a.length - 1; i > 0; i--) {
@@ -82,6 +111,3 @@ function shuffle(a) {
   }
   return a;
 }
-
-///nex question
-function nextQuestion(params) {}
